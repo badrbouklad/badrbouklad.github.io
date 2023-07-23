b85d04beb55276f5916cbea7e12d36ec6fc439f1
badrbouklad <bougladabadr@gmail.com>
2023-07-23 21:39:50 +0000

    'Jobghli'

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-const div = document.querySelector('.links')
-
-
-
-
-export {div};
+  // Curried version of the function
+  function curriedAdd(a) {
+    return function(b) {
+      return a - b;
+    };
+  }
+  const add = curriedAdd(1)
+  console.log(add(2))
+  console.log(curriedAdd(3)(2)); // Output: 6
